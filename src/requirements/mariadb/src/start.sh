2ad98664986f694dc127417ed0fb3870555c4504
Lomas Morgana <lmorgana@un-e2.kzn.21-school.ru>
2022-08-18 16:44:05 +0300

    works

--- src/requirements/mariadb/src/start.sh
+++ src/requirements/mariadb/src/start.sh
@@ -2,19 +2,19 @@
 
 echo "yep"
 #sleep 2
-#if [ ! -d "/var/lib/mysql/wordpress" ]; then
+if [ ! -d "/var/lib/mysql/wordpress" ]; then
 
-    mysql_install_db
+
     service mysql start
     echo "database start created"
     sleep 5
-    mysql -e "CREATE USER '${DB_LOGIN}'@'localhost' IDENTIFIED BY '${DB_PASS}';" &&\
+    mysql -e "CREATE USER '${DB_LOGIN}'@'%' identified by '${DB_PASS}';" &&\
    	mysql -e "CREATE DATABASE IF NOT EXISTS wordpress;" &&\
-   	mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${DB_LOGIN}'@'localhost' IDENTIFIED BY '${DB_PASS}';" &&\
+   	mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${DB_LOGIN}'@'%' identified by '${DB_PASS}';" &&\
    	mysql -e "FLUSH PRIVILEGES;"
 
-#    mysqladmin -u root password "{$ROOT_PASS}"
+#   mysqladmin -u root password "{$ROOT_PASS}"
     service mysql stop
     echo "database created"
-#fi
+fi
 mysqld_safe
