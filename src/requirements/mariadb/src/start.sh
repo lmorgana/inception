4c591e592378f7a5326500c22a33d179ec59a4ab
Lomas Morgana <lmorgana@un-e2.kzn.21-school.ru>
2022-08-27 17:00:05 +0300

    some

--- src/requirements/mariadb/src/start.sh
+++ src/requirements/mariadb/src/start.sh
@@ -1,10 +1,4 @@
-
-
-echo "yep"
-#sleep 2
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
-
-
     service mysql start
     echo "database start created"
     sleep 5
@@ -13,7 +7,6 @@ if [ ! -d "/var/lib/mysql/wordpress" ]; then
    	mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${DB_LOGIN}'@'%' identified by '${DB_PASS}';" &&\
    	mysql -e "FLUSH PRIVILEGES;"
 
-#   mysqladmin -u root password "{$ROOT_PASS}"
     service mysql stop
     echo "database created"
 fi
