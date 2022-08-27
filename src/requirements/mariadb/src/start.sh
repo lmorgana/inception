ac6ac5a2c0995e8daa6beb29e067e82b136e2255
Lomas Morgana <lmorgana@un-e2.kzn.21-school.ru>
2022-08-27 18:34:38 +0300

    afsdfa

--- src/requirements/mariadb/src/start.sh
+++ src/requirements/mariadb/src/start.sh
@@ -2,9 +2,11 @@ if [ ! -d "/var/lib/mysql/wordpress" ]; then
     service mysql start
     echo "database start created"
     sleep 5
-    mysql -e "CREATE USER '${DB_LOGIN}'@'%' identified by '${DB_PASS}';" &&\
-   	mysql -e "CREATE DATABASE IF NOT EXISTS wordpress;" &&\
-   	mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${DB_LOGIN}'@'%' identified by '${DB_PASS}';" &&\
+    mysql -e "CREATE USER '${DB_LOGIN}'@'localhost' identified by '${DB_PASS}';"
+    echo "mysql -e \"CREATE USER '${DB_LOGIN}'@'localhost' identified by '${DB_PASS}';\""
+   	mysql -e "CREATE DATABASE IF NOT EXISTS wordpress;"
+   	mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${DB_LOGIN}'@'%' identified by '${DB_PASS}';"
+   	echo "mysql -e \"GRANT ALL PRIVILEGES ON *.* TO '${DB_LOGIN}'@'%' identified by '${DB_PASS}';\""
    	mysql -e "FLUSH PRIVILEGES;"
 
     service mysql stop
